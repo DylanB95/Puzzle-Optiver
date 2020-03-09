030ff1947a1f1317b3c467e68dd6fa80e4fd0e1a
Dylan Beijers <dylanbeijers@gmail.com>
2020-03-09 10:32:08 +0100

    Added description of the puzzle

---
+++ solution.R
@@ -0,0 +1,13 @@
+# This R-file provides the solution to the challenge as provided by Optiver
+# The challenge goes as follows:
+#   Calvin has to cross several signals when he walks from his home to school. 
+#   Each of these signals operate independently. They alternate every 80 seconds 
+#   between green light and red light. At each signal, there is a counter display 
+#   that tells him how long it will be before the current signal light changes. 
+#   Calvin has a magic wand which lets him turn a signal from red to green 
+#   instantaneously. However, this wand comes with limited battery life, so he can 
+#   use it only for a specified number of times.
+
+#   Can you write a script that takes as inputs the number of signals and the number 
+#   of times Calvin can use his magic wand, and outputs the expected waiting time?
+
